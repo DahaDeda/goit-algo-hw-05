39f7b044a1ad8b5e70c9be800c99fb3572f04e1b
DahaDeda <146961908+DahaDeda@users.noreply.github.com>
2024-02-23 15:20:12 +0200

    1

---
+++ hw5_1.py
@@ -0,0 +1,17 @@
+import math
+    
+def catching_fibonacci():
+    cache = {}
+    def fibonacci(n):        
+        if n <= 0:
+            return 0
+        elif n == 1:
+            return 1
+        if n in cache:
+            return cache[n]
+        cache[n] = fibonacci(n-1) + fibonacci(n-2)
+        return cache[n]
+    return fibonacci
+fibonacci_function = catching_fibonacci()
+result = fibonacci_function(6)
+print(result)
